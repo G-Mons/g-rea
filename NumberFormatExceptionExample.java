04ea42af82d3d8a111d59bfdef208c48376f1a3d
G-Mons <ptkoon@gmail.com>
2023-03-16 11:05:13 +0900

    Update NumberFormatExceptionExample.java

--- NumberFormatExceptionExample.java
+++ NumberFormatExceptionExample.java
@@ -2,13 +2,13 @@ package sec01.exam04;
 
 public class NumberFormatExceptionExample {
 	public static void main(String[] args) {
+		//변수 설정
 		String data1 = "100";
 		String data2 = "a100";
-		//여기가 수정라인
-				
+		//할당
 		int value1 = Integer.parseInt(data1);
 		int value2 = Integer.parseInt(data2);
-		
+		//계산
 		int result = value1 + value2;
 		System.out.println(data1 + "+" + data2 + "=" + result);
 	}
